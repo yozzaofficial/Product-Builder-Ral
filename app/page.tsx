21aa1c4c6fb02b4e5b2c106ede50ecdcf5a7de45
yozzaofficial <matteo.granchelli@gmail.com>
2026-03-11 21:38:01 +0100

    fix css 2

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <div className="containerMain">
       <form id="formRedditoAnnuo" onSubmit={calcRal}>
         <div className="inputContainerAnnuo">
           <input
